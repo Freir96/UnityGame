c9a0482feeb27232f2e126bafcef6325f5556df1
Freir96 <a.gajewski6@gmail.com>
2018-08-03 17:53:01 +0200

    Add files via upload

---
+++ TalkGUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkGUI : MonoBehaviour {
+    bool isTalking;
+    private DisplayManager displayManager;
+    // Use this for initialization
+    void Start () {
+        isTalking = false;
+        //var boxCollider = gameObject.AddComponent<BoxCollider>();
+      //  boxCollider.isTrigger = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // Debug.Log("Hi");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isTalking == false)
+        {
+			isTalking = true;
+            displayManager.DisplayMessage("Hello Player!");
+            Debug.Log("entered");
+        }
+    }
+}
